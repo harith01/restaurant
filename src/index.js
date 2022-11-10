b3c3ae7c3ec3e36f9cdd8f5c966b3bf8a444ee84
harith01 <harithyusuph@gmail.com>
2022-11-10 16:35:44 +0100

    Add event listener to the page
    The event listener will listen for clicks on either the home, menu
    or contact button to call the appropriate function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
 
 import pageLoad from './pageLoad';
 import './style.css';
+import menu from './menu';
 
 
 pageLoad()
 
-const homeButton = document.querySelector('#home-btn');
-homeButton.addEventListener('click', ()=>{
+// const homeButton = document.querySelector('#home-btn');
+// homeButton.addEventListener('click', ()=>{
+//     const content = document.getElementById("content");
+//     while (content.hasChildNodes()) {
+//     content.removeChild(content.firstChild);
+//     }
+//   pageLoad()
+// });
+
+// const menuButton = document.querySelector('#menu-btn');
+// menuButton.addEventListener('click', ()=>{
+//     const content = document.getElementById("content");
+//     while (content.hasChildNodes()) {
+//         content.removeChild(content.firstChild);
+//     }
+//     menu()
+// });
+
+function clearScreen(){
     const content = document.getElementById("content");
-    while (content.hasChildNodes()) {
-    content.removeChild(list.firstChild);
-  }
-  pageLoad()
-});
+    while (content.hasChildNodes()){
+        content.removeChild(content.firstChild);
+    }
+}
+
+document.body.addEventListener('click', (e) => {
+    if (e.target.innerText == 'Home'){
+        clearScreen();
+        pageLoad();
+    }
+
+    if (e.target.innerText == 'Menu'){
+        clearScreen();
+        menu();
+    }
+
+    if (e.target.innerText == 'Contact'){
+        console.log('Contact')
+    }
+})
